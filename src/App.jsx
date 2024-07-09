8f7c52797bd18bcca1e8d101f4449b2d09d51644
ankit-2306 <bhardwajankit75168@gmail.com>
2024-07-09 19:50:18 +0530

    ready to render

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,42 @@ import './App.css'
 import Card from './assets/Card'
 
 function App() {
-  const [btcval, setBtcval] = useState(0);
-  const [dogeval, setDogeval] = useState(0);
-  const [ripval, setRipval] = useState(0);
+  const [btc,setBtc]=useState(0);
+  const [doge,setDoge]=useState(0);
+  const [rip,setRip]=useState(0);
   const [total,setTotal]=useState(0);
-
+  const [btcamt,setBtcamt]=useState(0.0);
+  const [dogeamt,setDogeamt]=useState(0.0);
+  const [ripamt,setRipamt]=useState(0.0);
+ 
   function handleSubmitt(name,amt){
+    console.log(name,amt);
+    let prevTotal=total;
     if(name=='BTC'){
-      setBtcval(()=>{return btcval+amt;});
+      let demo=(btc+amt)*25000;
+      let demo2=demo.toFixed(2);
+      setBtc((prev)=>prev+amt);
+      setBtcamt(()=>{ return demo2;});
+      prevTotal+=Number(demo2);
     }
     else if(name=='DOGE'){
-      setDogeval(()=>{return dogeval+amt;});
+      let demo=(doge+amt)*0.75;
+      let demo2=demo.toFixed(2);
+      setDoge((prev)=>prev+amt);
+      setDogeamt(()=>demo2);
+      prevTotal+=Number(demo2);
+      
+      
     }
-    else if(name=='RIPPLE'){
-      setRipval(()=>{return ripval+amt;})
+    else{
+      let demo=(rip+amt)*1.5;
+      let demo2=demo.toFixed(2);
+      setRip((prev)=>prev+amt);
+      setRipamt(()=>demo2);
+      prevTotal+=Number(demo2);
     }
-
-    setTotal(()=>(btcval+dogeval+ripval));
+    setTotal(()=>prevTotal);
+    console.log(prevTotal);
   }
 
   return (
@@ -31,23 +50,36 @@ function App() {
       <Card name="DOGE" vall={0.75} handleSubmit={handleSubmitt}/>
       <Card name="RIPPLE" vall={1.15} handleSubmit={handleSubmitt}/>
       </div>
-      <div className='text-center mt-5'>
+      <div className='text-center mt-20'>
         MY CART
       </div>
-          <span>COIN</span>
-          <span>QTY</span>
-          <span>AMOUNT</span>
-      <div>
-      <div>
-        {/* {btcval && } */}
+      <div className="flex justify-around">
+        <span>COIN</span>
+        <span>QNT.</span>
+        <span>AMOUNT</span>
+      </div>
         
-      </div>
-      <div>
-
-      </div>
-      <div>
-
-      </div>
+        <div className="flex justify-around ">
+          <span>BTC</span>
+          <span>{btc}</span>
+          <span>{btcamt}</span>
+        </div>
+      
+      
+        <div className=" flex justify-around">
+          <span>DOGE</span>
+          <span>{doge}</span>
+          <span>{dogeamt}</span>
+        </div>
+      
+        <div className='flex justify-around'>
+          <span>RIPPLE</span>
+          <span>{rip}</span>
+          <span>{ripamt}</span>
+        </div>
+      
+          
+      <div className='text-right mr-10'>
         Total:{total}
       </div>
         
--- src/assets/Card.jsx
+++ src/assets/Card.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react';
 
 const Card = ({name,vall,handleSubmit}) => {
     const [amt,setAmt]=useState(0);
-    function handleButton(name,amt){
-        if(amt){
-            handleSubmit(name,amt);
-        }
 
+    function handleButton(name,amt){
+        let demo=Number(amt);
+        
+            handleSubmit(name,demo);
+        
+        setAmt(()=>{return 0;})
     }
   return (
     <div>
@@ -23,7 +25,7 @@ const Card = ({name,vall,handleSubmit}) => {
             </section>
         </div>
         <input className="mx-5 mt-2" type="number" placeholder='Qty' value={amt!==0?amt:''} onChange={(e)=>{setAmt(e.target.value);}}/>
-        <button className="bg-blue-400 px-8 py-2 rounded-xl  " onClick={()=>{return handleButton(name,amt);}} >ADD</button>
+        <button className="bg-blue-400 px-8 py-2 rounded-xl  " onClick={()=>{ handleButton(name,amt);}} >ADD</button>
     </div>
   )
 }
